b742a5996a6e67b32efc8027ce1d37086bc630f9
Aslanyan99n <a99narek@gmail.com>
2023-07-05 10:57:30 +0400

    Initial Commit

---
+++ Honeymoon/HoneymoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneymoonApp.swift
+//  Honeymoon
+//
+//  Created by Narek on 05.07.23.
+//
+
+import SwiftUI
+
+@main
+struct HoneymoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
